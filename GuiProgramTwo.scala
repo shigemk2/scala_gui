64c6f6cefa1f1f7c241fee03957bfbdadd3d5ab8
shigemk2 <i.am.shige@gmail.com>
2015-09-08 20:44:45 +0900

    イベントを仕掛ける

---
+++ GuiProgramTwo.scala
@@ -0,0 +1,15 @@
+import scala.swing._
+
+class SampleUI extends MainFrame {
+  title = "GUI Program #2"
+  preferredSize = new Dimension(320, 240)
+  // Buttonの中身はボタンがクリックされて初めて呼ばれる
+  contents = Button("Press me, please") { println("Thank you") }
+}
+
+object GuiProgramTwo {
+  def main(args: Array[String]) {
+    val ui = new SampleUI
+    ui.visible = true
+  }
+}
