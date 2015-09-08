6961be26427b393015e1a46221becd23ebd99ec0
shigemk2 <i.am.shige@gmail.com>
2015-09-08 21:46:48 +0900

    代替プログラム

--- src/main/scala/GuiProgramThree.scala
+++ src/main/scala/GuiProgramThree.scala
@@ -2,11 +2,20 @@ import scala.swing._
 
 class SampleUI3 extends MainFrame {
   title = "GUI Program #3"
+  // 複数のコンポーネントをウィンドウに入れたい場合は、
+  // scala.swing.BoxPanelを使う
   contents = new BoxPanel(Orientation.Vertical) {
     contents += new Label("Look at me!")
     contents += Button("Press me, please") { println("Thank you") }
     contents += Button("Close") { sys.exit(0) }
   }
+
+  // 代わりにこのように書くことも出来る
+  // val box = new BoxPanel(Orientation.Vertical)
+  // box.contents += new Label("Look at me!")
+  // box.contents += Button("Press me, please") { println("Thank you") }
+  // box.contents += Button("Close") { sys.exit(0) }
+  // contents = box
 }
 
 object GuiProgramThree {
